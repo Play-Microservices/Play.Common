32e011c632cc0d971f265807c79cd139c472e225
Furmi <16333282+Furrman@users.noreply.github.com>
2026-06-29 11:34:25 +0100

    Add custom retry configurator to RabbitMQ

--- src/Play.Common/MassTransit/Extensions.cs
+++ src/Play.Common/MassTransit/Extensions.cs
@@ -9,26 +9,33 @@ namespace Play.Common.MassTransit;
 
 public static class Extensions
 {
-    public static WebApplicationBuilder AddMassTransitWithRabbitMQ(this WebApplicationBuilder builder)
+    public static WebApplicationBuilder AddMassTransitWithRabbitMQ(
+        this WebApplicationBuilder builder,
+        Action<IRetryConfigurator>? configureRetries = null)
     {
-        builder.Services.AddMassTransit(configure => 
+        builder.Services.AddMassTransit(configure =>
         {
             configure.AddConsumers(Assembly.GetEntryAssembly());
 
-            configure.UsingRabbitMq((context, configurator) => 
-            {
-                var configuration = context.GetService<IConfiguration>();
-                var serviceSettings = configuration!.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
-                var rabbitMQSettings = configuration!.GetSection(nameof(RabbitMQSettings)).Get<RabbitMQSettings>();
-                configurator.Host(rabbitMQSettings!.Host);
-                configurator.ConfigureEndpoints(context, new KebabCaseEndpointNameFormatter(serviceSettings!.ServiceName, false));
-                configurator.UseMessageRetry(retryConfigurator =>
-                {
-                    retryConfigurator.Interval(3, TimeSpan.FromSeconds(5));
-                });
-            });
+            UsingPlayEconomyRabbitMQ(configure, configureRetries);
         });
 
         return builder;
+    }
+
+    public static void UsingPlayEconomyRabbitMQ(
+        this IBusRegistrationConfigurator configure,
+        Action<IRetryConfigurator>? configureRetries = null)
+    {
+        configure.UsingRabbitMq((context, configurator) => 
+        {
+            var configuration = context.GetService<IConfiguration>();
+            var serviceSettings = configuration!.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
+            var rabbitMQSettings = configuration!.GetSection(nameof(RabbitMQSettings)).Get<RabbitMQSettings>();
+            configurator.Host(rabbitMQSettings!.Host);
+            configurator.ConfigureEndpoints(context, new KebabCaseEndpointNameFormatter(serviceSettings!.ServiceName, false));
+            configureRetries ??= retryConfigurator => { retryConfigurator.Interval(3, TimeSpan.FromSeconds(5)); };
+            configurator.UseMessageRetry(configureRetries);
+        });
     }
 }
